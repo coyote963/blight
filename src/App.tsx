b28e614fd77e072e9fdae3c2c93b280cc42b3544
c b <john.luan@gmail.com>
2021-03-09 21:10:28 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Example from './components/home'
+function App() {
+  return (
+    <div>
+      <Example></Example>
+    </div>
+  );
+}
+
+export default App;
